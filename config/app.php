7a71f8065ad56b7757f1eb8ce4b51b1593405954
wangchifu <wangchifu@gmail.com>
2019-02-27 16:27:58 +0800

    fix bug

--- config/app.php
+++ config/app.php
@@ -225,6 +225,7 @@ return [
     'database'=>[
         //網址轉換資料庫代碼
         'localhost'=>'s074600',
+        'www2.hdes.chc.edu.tw'=>'s074998',
         'chcschool.localhost'=>'s074999',
         'www.hdes.chc.edu.tw'=>'s074628',
     ],
