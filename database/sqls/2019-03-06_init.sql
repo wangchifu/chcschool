7bf0188618ff33a0dd8740d157efdc159f1b51cb
wangchifu <wangchifu@gmail.com>
2019-03-27 22:30:51 +0800

    fix bug

--- database/sqls/2019-03-06_init.sql
+++ database/sqls/2019-03-06_init.sql
@@ -8,10 +8,32 @@ CREATE TABLE `blocks` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
 INSERT INTO `blocks` (`id`, `title`, `content`, `order_by`, `setup_col_id`, `created_at`, `updated_at`) VALUES
-(1, '最新公告(系統區塊)', ' ', 1, 2, '2019-03-06 03:34:56', '2019-03-06 03:34:56'),
-(2, '連結區塊', '<ul><li><a href="http://boe.chc.edu.tw" target="_blank">教育處雲端</a></li><li><a href="http://school.chc.edu.tw" target="_blank">學校資料平台</a></li></ul>', 1, 1, '2019-03-06 03:34:56', '2019-03-06 03:34:56');
+(1, '最新公告(系統區塊)', ' ', 1, 2, '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(2, '連結區塊', '<ul><li><a href=\"http://boe.chc.edu.tw\" target=\"_blank\">教育處雲端</a></li><li><a href=\"http://school.chc.edu.tw\" target=\"_blank\">學校資料平台</a></li></ul>', 1, 1, '2019-03-27 11:25:37', '2019-03-27 11:25:37');
+
+CREATE TABLE `calendars` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `calendar_week_id` int(10) UNSIGNED NOT NULL,
+  `semester` int(10) UNSIGNED NOT NULL,
+  `calendar_kind` tinyint(4) NOT NULL,
+  `content` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `job_title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_by` int(10) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `calendar_weeks` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `semester` int(10) UNSIGNED NOT NULL,
+  `week` int(10) UNSIGNED NOT NULL,
+  `start_end` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 CREATE TABLE `contents` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -20,6 +42,17 @@ CREATE TABLE `contents` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `departments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_by` int(10) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 CREATE TABLE `fixes` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -33,14 +66,109 @@ CREATE TABLE `fixes` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
+CREATE TABLE `groups` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `disable` tinyint(4) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+INSERT INTO `groups` (`id`, `name`, `disable`, `created_at`, `updated_at`) VALUES
+(1, '行政人員', NULL, NULL, NULL),
+(2, '級任老師', NULL, NULL, NULL),
+(3, '科任老師', NULL, NULL, NULL),
+(4, '其他職員', NULL, NULL, NULL);
+
+
 CREATE TABLE `links` (
   `id` int(10) UNSIGNED NOT NULL,
+  `type_id` int(10) UNSIGNED NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `url` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `order_by` int(10) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `lunch_factories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `teacher_money` double(8,2) NOT NULL,
+  `disable` tinyint(4) DEFAULT NULL,
+  `fid` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fpwd` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `lunch_orders` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `semester` int(10) UNSIGNED NOT NULL,
+  `rece_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rece_date` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rece_no` int(10) UNSIGNED NOT NULL,
+  `rece_num` int(10) UNSIGNED NOT NULL,
+  `order_ps` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `lunch_order_dates` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `order_date` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `enable` tinyint(4) NOT NULL,
+  `semester` int(10) UNSIGNED NOT NULL,
+  `lunch_order_id` int(10) UNSIGNED NOT NULL,
+  `date_ps` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `lunch_places` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `disable` tinyint(4) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `lunch_setups` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `semester` int(10) UNSIGNED NOT NULL,
+  `die_line` tinyint(4) NOT NULL,
+  `teacher_open` tinyint(4) DEFAULT NULL,
+  `disable` tinyint(4) DEFAULT NULL,
+  `all_rece_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `all_rece_date` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `all_rece_no` int(10) UNSIGNED NOT NULL,
+  `all_rece_num` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `lunch_tea_dates` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `order_date` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `enable` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `semester` int(10) UNSIGNED NOT NULL,
+  `lunch_order_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `lunch_place_id` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lunch_factory_id` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `eat_style` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 CREATE TABLE `meetings` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -50,49 +178,71 @@ CREATE TABLE `meetings` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(18, '2014_10_12_000000_create_users_table', 1),
-(19, '2014_10_12_100000_create_password_resets_table', 1),
-(20, '2019_02_24_200737_create_setups_table', 1),
-(21, '2019_02_24_223237_create_setup_col_table', 1),
-(22, '2019_02_25_141610_create_posts_table', 1),
-(23, '2019_02_27_135346_create_table_contents', 1),
-(24, '2019_02_27_155304_create_links_table', 1),
-(25, '2019_02_27_220556_create_blocks_table', 1),
-(26, '2019_02_27_221945_create_sqls_table', 1),
-(27, '2019_03_04_144934_create_modules_table', 1),
-(29, '2019_03_05_090835_create_uploads_table', 2),
-(30, '2019_03_05_142735_create_fixes_table', 3),
-(31, '2019_03_05_150943_create_table_meetings', 4),
-(32, '2019_03_05_161053_create_table_reports', 4);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_02_24_200737_create_setups_table', 1),
+(4, '2019_02_24_223237_create_setup_col_table', 1),
+(5, '2019_02_25_141610_create_posts_table', 1),
+(6, '2019_02_27_135346_create_table_contents', 1),
+(7, '2019_02_27_155304_create_links_table', 1),
+(8, '2019_02_27_220556_create_blocks_table', 1),
+(9, '2019_02_27_221945_create_sqls_table', 1),
+(10, '2019_03_04_144934_create_modules_table', 1),
+(11, '2019_03_05_090835_create_uploads_table', 1),
+(12, '2019_03_05_142735_create_fixes_table', 1),
+(13, '2019_03_05_150943_create_table_meetings', 1),
+(14, '2019_03_05_161053_create_table_reports', 1),
+(15, '2019_03_21_200739_create_groups_table', 1),
+(16, '2019_03_21_200848_create_users_groups_table', 1),
+(17, '2019_03_22_221041_create_type_table', 1),
+(18, '2019_03_23_221611_create_user_powers', 1),
+(19, '2019_03_24_215036_create_departments_table', 1),
+(20, '2019_03_25_095351_create_calendars_table', 1),
+(21, '2019_03_25_095455_create_calendar_weeks_table', 1),
+(22, '2019_03_25_135149_create_lunch_setups_table', 1),
+(23, '2019_03_25_135209_create_lunch_orders_table', 1),
+(24, '2019_03_25_141135_create_lunch_factories_table', 1),
+(25, '2019_03_25_141149_create_lunch_places_table', 1),
+(26, '2019_03_25_152624_create_lunch_order_dates_table', 1),
+(27, '2019_03_25_160710_create_lunch_tea_dates', 1);
+
 
 CREATE TABLE `modules` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `active` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `active` text COLLATE utf8mb4_unicode_ci,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 INSERT INTO `modules` (`id`, `name`, `active`, `created_at`, `updated_at`) VALUES
-(1, '公告系統', '1', '2019-03-06 03:34:56', '2019-03-06 03:34:56'),
-(2, '檔案庫', '1', '2019-03-06 03:34:56', '2019-03-06 03:34:56'),
-(3, '好站連結', '1', '2019-03-06 03:34:56', '2019-03-06 03:34:56'),
-(4, '校務行政', '1', '2019-03-06 03:34:56', '2019-03-06 03:34:56'),
-(5, '會議文稿', '1', '2019-03-06 03:34:56', '2019-03-06 03:34:56'),
-(6, '報修系統', '1', '2019-03-06 03:34:56', '2019-03-06 03:34:56');
+(1, '公告系統', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(2, '檔案庫', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(3, '處室介紹', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(4, '好站連結', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(5, '校務行政', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(6, '會議文稿', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(7, '報修系統', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(8, '午餐系統', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(9, '校務行事曆', '1', '2019-03-27 11:25:37', '2019-03-27 11:25:37');
+
 
 CREATE TABLE `password_resets` (
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 CREATE TABLE `posts` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -108,6 +258,7 @@ CREATE TABLE `posts` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 CREATE TABLE `reports` (
   `id` int(10) UNSIGNED NOT NULL,
   `user_id` int(10) UNSIGNED NOT NULL,
@@ -119,6 +270,7 @@ CREATE TABLE `reports` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 CREATE TABLE `setups` (
   `id` int(10) UNSIGNED NOT NULL,
   `site_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -129,8 +281,10 @@ CREATE TABLE `setups` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 INSERT INTO `setups` (`id`, `site_name`, `nav_color`, `title_image`, `views`, `created_at`, `updated_at`) VALUES
-(1, '彰化縣xx國小全球資訊網', NULL, 1, 0, '2019-03-06 03:34:56', '2019-03-06 03:34:56');
+(1, '彰化縣xx國小全球資訊網', NULL, 1, 0, '2019-03-27 11:25:37', '2019-03-27 11:25:37');
+
 
 CREATE TABLE `setup_cols` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -139,9 +293,11 @@ CREATE TABLE `setup_cols` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 INSERT INTO `setup_cols` (`id`, `num`, `created_at`, `updated_at`) VALUES
-(1, 2, '2019-03-06 03:34:56', '2019-03-06 03:34:56'),
-(2, 10, '2019-03-06 03:34:56', '2019-03-06 03:34:56');
+(1, 2, '2019-03-27 11:25:37', '2019-03-27 11:25:37'),
+(2, 10, '2019-03-27 11:25:37', '2019-03-27 11:25:37');
+
 
 CREATE TABLE `sqls` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -151,8 +307,19 @@ CREATE TABLE `sqls` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 INSERT INTO `sqls` (`id`, `name`, `install`, `created_at`, `updated_at`) VALUES
-(1, '2019-03-06_init.sql', 1, '2019-03-06 03:34:56', '2019-03-06 03:34:56');
+(1, '2019-03-06_init.sql', 1, '2019-03-27 11:25:37', '2019-03-27 11:25:37');
+
+
+CREATE TABLE `types` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_by` int(10) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 CREATE TABLE `uploads` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -164,12 +331,12 @@ CREATE TABLE `uploads` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
   `username` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `order_by` int(10) UNSIGNED DEFAULT NULL,
-  `group_id` int(10) UNSIGNED DEFAULT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -185,19 +352,72 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `users` (`id`, `username`, `name`, `order_by`, `group_id`, `email`, `email_verified_at`, `password`, `admin`, `code`, `school`, `kind`, `title`, `login_type`, `disable`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '系統管理員', NULL, 99, NULL, NULL, '$2y$10$/rVJdRwHqjft4yqyyjYUBOnkQrwi9F9m01xRHBqg7EhdoYV1/JMrG', 1, NULL, NULL, NULL, NULL, 'local', NULL, NULL, '2019-03-06 03:34:56', '2019-03-06 03:34:56');
+
+INSERT INTO `users` (`id`, `username`, `name`, `order_by`, `email`, `email_verified_at`, `password`, `admin`, `code`, `school`, `kind`, `title`, `login_type`, `disable`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '系統管理員', NULL, NULL, NULL, '$2y$10$cVH9txmwAog4K3g5AmbjheZaKBeLGbVcUGkcDxAWyJt8fllOAeCTS', 1, NULL, NULL, NULL, NULL, 'local', NULL, NULL, '2019-03-27 11:25:37', '2019-03-27 11:25:37');
+
+
+CREATE TABLE `users_groups` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `group_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `user_powers` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 ALTER TABLE `blocks`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `calendars`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `calendar_weeks`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `contents`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `fixes`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `links`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `lunch_factories`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `lunch_orders`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `lunch_orders_name_unique` (`name`);
+
+ALTER TABLE `lunch_order_dates`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `lunch_order_dates_order_date_unique` (`order_date`);
+
+ALTER TABLE `lunch_places`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `lunch_setups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `lunch_setups_semester_unique` (`semester`);
+
+ALTER TABLE `lunch_tea_dates`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `meetings`
@@ -227,32 +447,71 @@ ALTER TABLE `setup_cols`
 ALTER TABLE `sqls`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `uploads`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `users_groups`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `user_powers`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `blocks`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+ALTER TABLE `calendars`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `calendar_weeks`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `contents`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `departments`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `fixes`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+ALTER TABLE `groups`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 ALTER TABLE `links`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `lunch_factories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `lunch_orders`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `lunch_order_dates`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `lunch_places`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `lunch_setups`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `lunch_tea_dates`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `meetings`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 ALTER TABLE `modules`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 ALTER TABLE `posts`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
@@ -269,8 +528,17 @@ ALTER TABLE `setup_cols`
 ALTER TABLE `sqls`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+ALTER TABLE `types`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `uploads`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `users_groups`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `user_powers`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
