5ee25bdcf368fe4f39ea235adb45bb847dcb0de3
wangchifu <wangchifu@gmail.com>
2019-05-04 00:34:16 +0800

    fix bug

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,7 +25,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title'){{ $setup->site_name }}</title>
     <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
         <script src="{{ asset('js/jquery.validate.min.js') }}"></script>
