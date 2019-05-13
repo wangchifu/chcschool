4f1cbf3d25d21e3327469971139d09045017b167
wangchifu <wangchifu@gmail.com>
2019-05-13 12:31:26 +0800

    fix bug

--- resources/views/setups/edit_block.blade.php
+++ resources/views/setups/edit_block.blade.php
@@ -41,7 +41,7 @@
             </td>
         </tr>
     </table>
-    <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
+    <script src="{{ asset('mycke/ckeditor.js') }}"></script>
     <script>
         CKEDITOR.replace('my-editor'
             ,{
