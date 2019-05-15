71282060d40ed40c16691ce43830254dd61df252
wangchifu <wangchifu@gmail.com>
2019-05-15 16:40:48 +0800

    fix bug

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'admin_exec' => \App\Http\Middleware\AdminExecMiddleware::class,
         'exec' => \App\Http\Middleware\ExecMiddleware::class,
     ];
 
--- routes/web.php
+++ routes/web.php
@@ -235,16 +235,19 @@ Route::group(['middleware' => 'exec'],function(){
     Route::get('departments_exec/{department}/edit', 'DepartmentController@exec_edit')->name('departments.exec_edit');
     Route::patch('departments_exec/{department}', 'DepartmentController@exec_update')->name('departments.exec_update');
 
+});
+
+//行政人員及管理者
+Route::group(['middleware' => 'admin_exec'],function(){
+
     Route::get('/laravel-filemanager', '\UniSharp\LaravelFilemanager\Controllers\LfmController@show');
     Route::post('/laravel-filemanager/upload', '\UniSharp\LaravelFilemanager\Controllers\UploadController@upload');
 
 });
 
+
 //管理者可用
 Route::group(['middleware' => 'admin'],function(){
-    Route::get('/laravel-filemanager', '\UniSharp\LaravelFilemanager\Controllers\LfmController@show');
-    Route::post('/laravel-filemanager/upload', '\UniSharp\LaravelFilemanager\Controllers\UploadController@upload');
-
     //更改密碼
     Route::get('edit_password','HomeController@edit_password')->name('edit_password');
     Route::patch('update_password','HomeController@update_password')->name('update_password');
