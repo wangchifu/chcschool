ca8a18075d543ac5c9e2d3e8fc2f6f329fa5c102
wangchifu <wangchifu@gmail.com>
2019-06-20 14:05:12 +0800

    fix bug

--- resources/views/layouts/photo_link.blade.php
+++ resources/views/layouts/photo_link.blade.php
@@ -1,14 +1,14 @@
 <div class="container-fluid">
     <div class="row">
         @foreach($photo_links as $photo_link)
-            <div class="col-3 col-sm-2 col-md-2 col-lg-1">
+            <div class="float-left" style="margin: 5px;">
                 <?php
                 $school_code = school_code();
                 $img = "storage/".$school_code.'/photo_links/'.$photo_link->image;
                 ?>
                 <figure class="figure">
-                    <a href="{{ $photo_link->url }}" target="_blank"><img src="{{ asset($img) }}" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure."></a>
-                    <figcaption class="figure-caption">{{ $photo_link->name }}</figcaption>
+                    <a href="{{ $photo_link->url }}" target="_blank"><img src="{{ asset($img) }}" class="figure-img img-fluid rounded" width="100"></a>
+                    <figcaption class="figure-caption"><small>{{ $photo_link->name }}</small></figcaption>
                 </figure>
             </div>
         @endforeach
