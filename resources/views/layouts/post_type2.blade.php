7395b1f3dbef73d87405d8c4fae8ead897454e17
wangchifu <wangchifu@hdes.chc.edu.tw>
2021-07-15 10:29:01 +0800

    分類公告改標籤式

--- resources/views/layouts/post_type2.blade.php
+++ resources/views/layouts/post_type2.blade.php
@@ -26,147 +26,163 @@ $posts = \App\Post::where('insite',null)
     }
 
 </style>
-<h4 class="text-info"><strong>一般公告</strong></h4>
-<table class="table table-striped" style="word-break: break-all;">
-    <tbody>
-    @foreach($posts as $post)
-        <tr>
-            <td>
-                <h4>
-                    @if($post->top)
-                        <p class="badge badge-danger">置頂</p>
-                    @endif
-                        <?php
-                        if($post->insite==1){
-                            if(auth()->check()){
-                                $can_see = 1;
-                            }else{
-                                $can_see = 0;
-                            }
-                        }else{
-                            $can_see = 1;
-                        };
-                        $school_code = school_code();
-                        $title = str_limit($post->title,80);
-                        //有無附件
-                        $files = get_files(storage_path('app/public/'.$school_code.'/posts/'.$post->id.'/files'));
-                        ?>
-                        @if($can_see)
-                            @if($post->insite==1)
-                                <span class="text-danger">[ 內部公告 ]</span>
-                            @endif
-                            <a href="{{ route('posts.show',$post->id) }}">{{ $title }}</a>
-                        @else
-                            <span class='text-danger'>[ 內部公告 ]</span>
-                            {{ $title }}
-                        @endif
-                </h4>
-
-                <?php
-                $content = str_limit(strip_tags($post->content),'150');
-                $content = str_replace('&nbsp;','',$content);
-                ?>
-                @if($post->title_image)
-                    <a href="{{ route('posts.show',$post->id) }}">
-                        <img src="{{ asset('storage/'.$school_code.'/posts/'.$post->id.'/title_image.png') }}" class="image2 img-fluid rounded" width="100px">
-                    </a>
-                @endif
-                <p class="pp1">
-                    {{ $content }}
-                    <br>
-                    <small class="text-secondary">
-                        <a href="{{ route('posts.job_title',$post->job_title) }}">{{ $post->job_title }}</a> / {{ $post->created_at }} / 點閱：{{ $post->views }}
-                        @if(!empty($files))
-                            <span class="text-info"><i class="fas fa-file"></i> [附件]</span>
-                        @endif
-                    </small>
-                </p>
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
-<a href="{{ route('posts.type',0) }}"><small><i class="far fa-hand-point-up"></i> 更多 一般公告...</small></a>
-<hr>
-@foreach($post_types as $post_type)
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">一般公告</a>
+    </li>
     <?php
-    $posts = \App\Post::where('insite',$post_type->id)
-        ->orderBy('top','DESC')
-        ->orderBy('created_at','DESC')
-        ->paginate(10);
+        $p=1;
     ?>
-<h4 class="text-info"><strong>{{ $post_type->name }}</strong></h4>
-<table class="table table-striped" style="word-break: break-all;">
-    <tbody>
-    @foreach($posts as $post)
-        <tr>
-            <td>
-                <h4>
-                    @if($post->top)
-                        <p class="badge badge-danger">置頂</p>
-                    @endif
-                    <?php
-                    if($post->insite==1){
-                        if(auth()->check()){
-                            $can_see = 1;
-                        }else{
-                            $can_see = 0;
-                        }
-                    }else{
-                        $can_see = 1;
-                    };
-                    $school_code = school_code();
-                    $title = str_limit($post->title,80);
-                    //有無附件
-                    $files = get_files(storage_path('app/public/'.$school_code.'/posts/'.$post->id.'/files'));
-                    ?>
-                    @if($can_see)
-                        @if($post->insite==1)
-                            <span class="text-danger">[ 內部公告 ]</span>
-                        @endif
-                        <a href="{{ route('posts.show',$post->id) }}">{{ $title }}</a>
-                    @else
-                        <span class='text-danger'>[ 內部公告 ]</span>
-                        {{ $title }}
-                    @endif
-                </h4>
-                <?php
-                $content = str_limit(strip_tags($post->content),'150');
-                $content = str_replace('&nbsp;','',$content);
-                ?>
-                @if($can_see)
-                    @if($post->title_image)
-                        <a href="{{ route('posts.show',$post->id) }}">
-                            <img src="{{ asset('storage/'.$school_code.'/posts/'.$post->id.'/title_image.png') }}" class="image2 img-fluid rounded" width="100px">
-                        </a>
-                    @endif
-                @endif
-                <p class="pp1">
-                    @if($can_see)
-                        {{ $content }}
-                    @else
-                        <span>請登入後再查看</span>
-                    @endif
-                    <br>
-                    <small class="text-secondary">
-                        <a href="{{ route('posts.job_title',$post->job_title) }}">{{ $post->job_title }}</a> / {{ $post->created_at }} / 點閱：{{ $post->views }}
-                        @if(!empty($files))
-                            <span class="text-info"><i class="fas fa-file"></i> [附件]</span>
-                        @endif
-                    </small>
-                </p>
-            </td>
-        </tr>
+    @foreach($post_types as $post_type)
+        <?php
+            $p++;
+        ?>
+        <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile{{ $p }}" role="tab" aria-controls="profile" aria-selected="false">{{ $post_type->name }}</a>
+        </li>
     @endforeach
-    </tbody>
-</table>
+</ul>
+<div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab" style="margin: 10px;">
+        <table class="table table-striped" style="word-break: break-all;">
+            <tbody>
+            @foreach($posts as $post)
+                <tr>
+                    <td>
+                        <h5>
+                            @if($post->top)
+                                <p class="badge badge-danger">置頂</p>
+                            @endif
+                            <?php
+                            if($post->insite==1){
+                                if(auth()->check()){
+                                    $can_see = 1;
+                                }else{
+                                    $can_see = 0;
+                                }
+                            }else{
+                                $can_see = 1;
+                            };
+                            $school_code = school_code();
+                            $title = str_limit($post->title,80);
+                            //有無附件
+                            $files = get_files(storage_path('app/public/'.$school_code.'/posts/'.$post->id.'/files'));
+                            ?>
+                            @if($can_see)
+                                @if($post->insite==1)
+                                    <span class="text-danger">[ 內部公告 ]</span>
+                                @endif
+                                <a href="{{ route('posts.show',$post->id) }}">{{ $title }}</a>
+                            @else
+                                <span class='text-danger'>[ 內部公告 ]</span>
+                                {{ $title }}
+                            @endif
+                        </h5>
 
+                        <?php
+                        $content = str_limit(strip_tags($post->content),'150');
+                        $content = str_replace('&nbsp;','',$content);
+                        ?>
+                        @if($post->title_image)
+                            <a href="{{ route('posts.show',$post->id) }}">
+                                <img src="{{ asset('storage/'.$school_code.'/posts/'.$post->id.'/title_image.png') }}" class="image2 img-fluid rounded" width="100px">
+                            </a>
+                        @endif
+                        <p class="pp1">
+                            {{ $content }}
+                            <br>
+                            <small class="text-secondary">
+                                <a href="{{ route('posts.job_title',$post->job_title) }}">{{ $post->job_title }}</a> / {{ $post->created_at }} / 點閱：{{ $post->views }}
+                                @if(!empty($files))
+                                    <span class="text-info"><i class="fas fa-file"></i> [附件]</span>
+                                @endif
+                            </small>
+                        </p>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <a href="{{ route('posts.type',0) }}"><small><i class="far fa-hand-point-up"></i> 更多 一般公告...</small></a>
+    </div>
 
-
-
-
-
-
-<a href="{{ route('posts.type',$post_type->id) }}"><small><i class="far fa-hand-point-up"></i> 更多 {{ $post_type->name }}...</small></a>
-<hr>
-@endforeach
+    <?php
+        $p=2;
+    ?>
+    @foreach($post_types as $post_type)
+        <div class="tab-pane fade" id="profile{{ $p }}" role="tabpanel" aria-labelledby="profile-tab" style="margin: 10px;">
+            <?php
+            $p++;
+            $posts = \App\Post::where('insite',$post_type->id)
+                ->orderBy('top','DESC')
+                ->orderBy('created_at','DESC')
+                ->paginate(10);
+            ?>
+            <table class="table table-striped" style="word-break: break-all;">
+                <tbody>
+                @foreach($posts as $post)
+                    <tr>
+                        <td>
+                            <h5>
+                                @if($post->top)
+                                    <p class="badge badge-danger">置頂</p>
+                                @endif
+                                <?php
+                                if($post->insite==1){
+                                    if(auth()->check()){
+                                        $can_see = 1;
+                                    }else{
+                                        $can_see = 0;
+                                    }
+                                }else{
+                                    $can_see = 1;
+                                };
+                                $school_code = school_code();
+                                $title = str_limit($post->title,80);
+                                //有無附件
+                                $files = get_files(storage_path('app/public/'.$school_code.'/posts/'.$post->id.'/files'));
+                                ?>
+                                @if($can_see)
+                                    @if($post->insite==1)
+                                        <span class="text-danger">[ 內部公告 ]</span>
+                                    @endif
+                                    <a href="{{ route('posts.show',$post->id) }}">{{ $title }}</a>
+                                @else
+                                    <span class='text-danger'>[ 內部公告 ]</span>
+                                    {{ $title }}
+                                @endif
+                            </h5>
+                            <?php
+                            $content = str_limit(strip_tags($post->content),'150');
+                            $content = str_replace('&nbsp;','',$content);
+                            ?>
+                            @if($can_see)
+                                @if($post->title_image)
+                                    <a href="{{ route('posts.show',$post->id) }}">
+                                        <img src="{{ asset('storage/'.$school_code.'/posts/'.$post->id.'/title_image.png') }}" class="image2 img-fluid rounded" width="100px">
+                                    </a>
+                                @endif
+                            @endif
+                            <p class="pp1">
+                                @if($can_see)
+                                    {{ $content }}
+                                @else
+                                    <span>請登入後再查看</span>
+                                @endif
+                                <br>
+                                <small class="text-secondary">
+                                    <a href="{{ route('posts.job_title',$post->job_title) }}">{{ $post->job_title }}</a> / {{ $post->created_at }} / 點閱：{{ $post->views }}
+                                    @if(!empty($files))
+                                        <span class="text-info"><i class="fas fa-file"></i> [附件]</span>
+                                    @endif
+                                </small>
+                            </p>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <a href="{{ route('posts.type',$post_type->id) }}"><small><i class="far fa-hand-point-up"></i> 更多 {{ $post_type->name }}...</small></a>
+        </div>
+    @endforeach
+</div>
