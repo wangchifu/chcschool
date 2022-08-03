cd6bcc90c9e1cb1d9e478c226ead569cd8a8c992
wangchifu <wangchifu@hdes.chc.edu.tw>
2022-08-03 22:04:52 +0800

    fix club bug

--- app/Http/Controllers/ClubsController.php
+++ app/Http/Controllers/ClubsController.php
@@ -407,7 +407,7 @@ class ClubsController extends Controller
                 $att['semester'] = $semester;
                 $att['no'] = $line['學號'];
                 $att['name'] = $line['姓名'];
-                $b = explode('/', $line['生日(西元)']);
+                $b = explode('/', $line['生日(西元)']->format('Y/m/d'));
                 $att['pwd'] = $b[0] . sprintf("%02s", $b[1]) . sprintf("%02s", $b[2]);
                 $att['class_num'] = $line['年級(數字)'] . sprintf("%02s", $line['年級(數字)']) . sprintf("%02s", $line['座號']);
                 $att['birthday'] = $att['pwd'];
