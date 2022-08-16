844393d188d5765fc5461760a61fb470f9c5c0d3
wangchifu <wangchifu@hdes.chc.edu.tw>
2022-08-16 08:42:51 +0800

    fix chc air

--- resources/views/layouts/chc_air.blade.php
+++ resources/views/layouts/chc_air.blade.php
@@ -28,7 +28,9 @@ if(file_exists('../../service/chc_air/download/'.$chk_file.'.txt')){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 3);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 3);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
     $html = curl_exec($ch);
     curl_close($ch);
     $data = json_decode($html);
@@ -43,7 +45,7 @@ if(file_exists('../../service/chc_air/download/'.$chk_file.'.txt')){
     fwrite($file_count,$count);
     fclose($file_count);
 
-    if(is_null($data)){
+    if(!isset($data->records)){
         $data = [];
         //$select_data=[];
         $air_data=[];
