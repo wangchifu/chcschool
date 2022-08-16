e73142042dcc6c5c5c43c857f2d877272dcd1fed
wangchifu <wangchifu@hdes.chc.edu.tw>
2022-08-16 08:56:13 +0800

    fix

--- resources/views/layouts/chc_air.blade.php
+++ resources/views/layouts/chc_air.blade.php
@@ -28,8 +28,8 @@ if(file_exists('../../service/chc_air/download/'.$chk_file.'.txt')){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 3);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 3);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 1);
     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
     $html = curl_exec($ch);
     curl_close($ch);
