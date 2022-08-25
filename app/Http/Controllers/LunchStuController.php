7b4713288a88e291241cbba74e61f83e6d4f95b9
wangchifu <wangchifu@hdes.chc.edu.tw>
2022-08-25 23:29:55 +0800

    fix lunch stu

--- app/Http/Controllers/LunchStuController.php
+++ app/Http/Controllers/LunchStuController.php
@@ -14,68 +14,83 @@ use Illuminate\Http\Request;
 
 class LunchStuController extends Controller
 {
-    public function index($semester = null)
+    public function index($lunch_order_id = null, $sample_date = null)
     {
         $admin = check_power('午餐系統', 'A', auth()->user()->id);
 
-        $semester_array = LunchSetup::orderBy('semester', 'DESC')
-            ->pluck('semester', 'semester')
-            ->toArray();
-        $semester = (empty($semester)) ? reset($semester_array) : $semester;
-
-        $club_student_num = ClubStudent::where('semester', $semester)->count();
-        $student_classes = [];
-
-        $lunch_class_dates = LunchClassDate::where('semester', $semester)
-            ->orderBy('lunch_class_id')
-            ->orderBy('order_date')
+        $lunch_orders = LunchOrder::orderBy('name', 'DESC')
             ->get();
-
-        $lunch_class_data = [];
-        foreach ($lunch_class_dates as $lunch_class_date) {
-            $lunch_class_data[$lunch_class_date->lunch_class_id][$lunch_class_date->order_date][1] = $lunch_class_date->eat_style1;
-            $lunch_class_data[$lunch_class_date->lunch_class_id][$lunch_class_date->order_date][4] = $lunch_class_date->eat_style4;
+        foreach ($lunch_orders as $lunch_order) {
+            $check = LunchClassDate::where('lunch_order_id', $lunch_order->id)->count();
+            $string = ($check > 0) ? "(已訂餐)" : "(未訂餐)";
+            $lunch_order_array[$lunch_order->id] = $lunch_order->name . $string;
         }
+        $lunch_order_id = (empty($lunch_order_id)) ? array_key_first($lunch_order_array) : $lunch_order_id;
+        $lunch_order = LunchOrder::find($lunch_order_id);
+        $semester = $lunch_order->semester;
+        $club_student_num = ClubStudent::where('semester', $semester)->count();
 
-        $lunch_orders = [];
-
+        $factory_array = LunchFactory::where('disable', null)
+            ->pluck('name', 'id')
+            ->toArray();
+        $lunch_class_data = [];
+        $student_classes = [];
         if (!empty($semester)) {
-            $lunch_orders = LunchOrder::where('semester', $semester)
-                ->orderBy('name')
+            $lunch_class_dates = LunchClassDate::where('lunch_order_id', $lunch_order_id)
+                ->orderBy('lunch_class_id')
+                ->orderBy('order_date')
                 ->get();
+            foreach ($lunch_class_dates as $lunch_class_date) {
+                $lunch_class_data[$lunch_class_date->lunch_class_id][$lunch_class_date->order_date][1] = $lunch_class_date->eat_style1;
+                $lunch_class_data[$lunch_class_date->lunch_class_id][$lunch_class_date->order_date][4] = $lunch_class_date->eat_style4;
+                $lunch_class_data[$lunch_class_date->lunch_class_id][$lunch_class_date->order_date]['factory'] = $factory_array[$lunch_class_date->lunch_factory_id];
+            }
+
             $student_classes = StudentClass::where('semester', $semester)
                 ->orderBy('student_year')
                 ->orderBy('student_class')
                 ->get();
         }
-        $factory_array = LunchFactory::where('disable', null)
-            ->pluck('name', 'id')
-            ->toArray();
 
+        $sample_data = [];
+        if (!empty($sample_date)) {
+            $sample_lunch_dates = LunchClassDate::where('order_date', $sample_date)
+                ->orderBy('lunch_class_id')
+                ->get();
+            foreach ($sample_lunch_dates as $sample_lunch_date) {
+                $sample_data[$sample_lunch_date->lunch_class_id][1] = $sample_lunch_date->eat_style1;
+                $sample_data[$sample_lunch_date->lunch_class_id][4] = $sample_lunch_date->eat_style4;
+            }
+        }
 
         $data = [
             'admin' => $admin,
+            'lunch_order_id' => $lunch_order_id,
             'semester' => $semester,
-            'semester_array' => $semester_array,
+            'lunch_order_array' => $lunch_order_array,
+            'lunch_order' => $lunch_order,
             'lunch_orders' => $lunch_orders,
             'student_classes' => $student_classes,
             'club_student_num' => $club_student_num,
             'factory_array' => $factory_array,
             'lunch_class_dates' => $lunch_class_dates,
             'lunch_class_data' => $lunch_class_data,
+            'sample_data' => $sample_data,
         ];
         return view('lunch_stus.index', $data);
     }
 
-    public function store(Request $request, $semester)
+    public function store(Request $request, $lunch_order_id)
     {
 
         $eat_data1 = $request->input('eat_data1');
         $eat_data4 = $request->input('eat_data4');
+
         $lunch_factory_id = $request->input('lunch_factory_id');
 
-
-        $lunch_order_dates = LunchOrderDate::where('semester', $semester)
+        $lunch_order = LunchOrder::find($lunch_order_id);
+        $semester = $lunch_order->semester;
+        $lunch_order_dates = LunchOrderDate::where('lunch_order_id', $lunch_order_id)
             ->orderBy('order_date')
             ->get();
         foreach ($lunch_order_dates as $lunch_order_date) {
@@ -88,7 +103,7 @@ class LunchStuController extends Controller
                     'order_date' => $lunch_order_date->order_date,
                     'semester' => $semester,
                     'lunch_order_id' => $lunch_order_date->lunch_order_id,
-                    'lunch_factory_id' => $lunch_factory_id,
+                    'lunch_factory_id' => $lunch_factory_id[$k],
                     'eat_style1' => $eat_style1,
                     'eat_style2' => null,
                     'eat_style3' => null,
@@ -102,16 +117,16 @@ class LunchStuController extends Controller
         }
 
 
-        return redirect()->route('lunch_stus.index', $semester);
+        return redirect()->route('lunch_stus.index', $lunch_order_id);
     }
 
-    public function delete($semester)
+    public function delete($lunch_order_id)
     {
         $admin = check_power('午餐系統', 'A', auth()->user()->id);
         if ($admin) {
-            LunchClassDate::where('semester', $semester)->delete();
+            LunchClassDate::where('lunch_order_id', $lunch_order_id)->delete();
         }
-        return redirect()->route('lunch_stus.index');
+        return redirect()->route('lunch_stus.index', $lunch_order_id);
     }
 
     public function change_num(Request $request)
--- routes/web.php
+++ routes/web.php
@@ -211,9 +211,9 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('lunch_lists/all_semester', 'LunchListController@all_semester')->name('lunch_lists.all_semester');
     Route::post('lunch_lists/semester_print', 'LunchListController@semester_print')->name('lunch_lists.semester_print');
 
-    Route::get('lunch_stus/index/{semester?}', 'LunchStuController@index')->name('lunch_stus.index');
+    Route::get('lunch_stus/index/{lunch_order_id?}/{sample_date?}', 'LunchStuController@index')->name('lunch_stus.index');
     Route::get('lunch_stus/delete/{semester}', 'LunchStuController@delete')->name('lunch_stus.delete');
-    Route::post('lunch_stus/store/{semester}', 'LunchStuController@store')->name('lunch_stus.store');
+    Route::post('lunch_stus/store/{lunch_order_id}', 'LunchStuController@store')->name('lunch_stus.store');
     Route::post('lunch_stus/change_num', 'LunchStuController@change_num')->name('lunch_stus.change_num');
     Route::post('lunch_stus/store_ps/{lunch_order}', 'LunchStuController@store_ps')->name('lunch_stus.store_ps');
 
