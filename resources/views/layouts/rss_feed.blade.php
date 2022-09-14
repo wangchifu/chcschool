68e893df0dd943856f9209d789962e8581ea3e74
wangchifu <wangchifu@hdes.chc.edu.tw>
2022-09-14 21:32:25 +0800

    fix rss

--- resources/views/layouts/rss_feed.blade.php
+++ resources/views/layouts/rss_feed.blade.php
@@ -14,20 +14,26 @@ $rss_feeds = \App\RssFeed::all();
     $i=1;
 	foreach ($rss->getElementsByTagName('item') as $node) {
         if($i>$rss_feed->num) break;
-		$item = array ( 
+        
+        if (!empty($node->getElementsByTagName('description')->item(0)->nodeValue)) {
+            $desc = $node->getElementsByTagName('description')->item(0)->nodeValue;
+        }else{
+            $desc = "說明";
+        }
+        
+        $item = array ( 
 			'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-			'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+			'desc' => $desc,
 			'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
 			//'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
 			);
 		array_push($feeds, $item);
         $i++;
+		
 	}
 ?>
+    <h4>{{ $rss_feed->title }}</h4>
     @if($rss_feed->type==1)
-    <button type="button" class="btn btn-primary">
-        {{ $rss_feed->title }} <span class="badge badge-light">{{ $rss_feed->num }}</span>
-    </button>
     <div class="list-group">
         @foreach($feeds as $k=>$v)
         <a href="{{ $v['link'] }}" target="_blank" class="list-group-item list-group-item-action">{{ $v['title'] }}</a>
@@ -35,9 +41,6 @@ $rss_feeds = \App\RssFeed::all();
     </div>
     @endif
     @if($rss_feed->type==2)
-    <button type="button" class="btn btn-primary">
-        {{ $rss_feed->title }} <span class="badge badge-light">{{ $rss_feed->num }}</span>
-    </button>
     <div class="row">
         @foreach($feeds as $k=>$v)
         <div class="col-2" style="margin-bottom: 10px;">    
