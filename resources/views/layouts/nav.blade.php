dcc6c4914af86132c3c2e2a5aeb3e5185d1c662b
wangchifu <wangchifu@gmail.com>
2024-02-28 20:51:06 +0800

    fix

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -9,7 +9,6 @@
 </style>
 <nav class="navbar navbar-expand-lg {{ $nav_color }}" id="mainNav">
     <div class="container-fluid">
-        <div class="table-responsive">
             <a href="#page-top" style="margin-right: 10px;">
                 @if(file_exists(storage_path('app/public/'.$school_code.'/title_image/logo.ico')))
                     <img src="{{ asset('storage/'.$school_code.'/title_image/logo.ico') }}" width="30" height="30" class="d-inline-block align-top" alt="">
@@ -18,7 +17,6 @@
                 @endif
             </a>
             <a class="navbar-brand js-scroll-trigger" href="{{  route('index') }}">{{ $setup->site_name }}</a>
-        </div>
         <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
