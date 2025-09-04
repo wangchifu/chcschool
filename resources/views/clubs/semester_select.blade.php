b246ec164848f145c318565cdf0f52d6b9a55221
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-09-04 21:17:26 +0800

    fix club

--- resources/views/clubs/semester_select.blade.php
+++ resources/views/clubs/semester_select.blade.php
@@ -17,13 +17,15 @@
                         $check_students[$club_semester->semester] = \App\ClubStudent::where('semester', $club_semester->semester)->count();                        
                     ?>
                     @if(date('YmdHi') >= str_replace('-','',$club_semester->start_date) and date('YmdHi') <= str_replace('-','',$club_semester->stop_date))
-                        <a href="{{ route('clubs.parents_login',['semester'=>$club_semester->semester,'class_id'=>'1']) }}" class="btn btn-primary">{{ $club_semester->semester }} 學期「學生特色社團」報名</a>
+                        <a href="{{ route('clubs.parents_login',['semester'=>$club_semester->semester,'class_id'=>'1']) }}" class="btn btn-primary">{{ $club_semester->semester }} 學期「學生特色社團」報名按這裡</a>
                         <a href="{{ route('clubs.show_clubs',['semester'=>$club_semester->semester,'class_id'=>'1']) }}" class="btn btn-info" target="_blank"> <i class="fas fa-hand-point-up"></i>社團一覽</a>
                         <br>
                         <small>報名時間：({{ $club_semester->start_date }} ~ {{ $club_semester->stop_date }})</small>
                     @else
                         @if(count($check_club1) >0)
+                        <!--
                         <span class="btn btn-secondary disabled">{{ $club_semester->semester }} 學期「學生特色社團」報名</span>
+                        -->
                         <a href="{{ route('clubs.show_clubs',['semester'=>$club_semester->semester,'class_id'=>'1']) }}" class="btn btn-info" target="_blank"><i class="fas fa-hand-point-up"></i> 社團一覽</a>
                         <br>
                         <small>報名時間：({{ $club_semester->start_date }} ~ {{ $club_semester->stop_date }})</small>                            
@@ -31,14 +33,16 @@
                     @endif
                     @if(date('YmdHi') >= str_replace('-','',$club_semester->start_date2) and date('YmdHi') <= str_replace('-','',$club_semester->stop_date2))
                         <hr>
-                        <a href="{{ route('clubs.parents_login',['semester'=>$club_semester->semester,'class_id'=>'2']) }}" class="btn btn-primary">{{ $club_semester->semester }} 學期「學生課後活動」報名</a>
+                        <a href="{{ route('clubs.parents_login',['semester'=>$club_semester->semester,'class_id'=>'2']) }}" class="btn btn-primary">{{ $club_semester->semester }} 學期「學生課後活動」報名按這裡</a>
                         <a href="{{ route('clubs.show_clubs',['semester'=>$club_semester->semester,'class_id'=>'2']) }}" class="btn btn-info" target="_blank"><i class="fas fa-hand-point-up"></i> 社團一覽</a>
                         <br>
                         <small>報名時間：({{ $club_semester->start_date2 }} ~ {{ $club_semester->stop_date2 }})</small>
                     @else
                         @if(count($check_club2) >0)
                         <hr>
-                        <span class="btn btn-secondary disabled">{{ $club_semester->semester }} 學期「學生課後活動」報名</span>
+                        <!--
+                            <span class="btn btn-secondary disabled">{{ $club_semester->semester }} 學期「學生課後活動」報名</span>
+                        -->
                         <a href="{{ route('clubs.show_clubs',['semester'=>$club_semester->semester,'class_id'=>'2']) }}" class="btn btn-info" target="_blank"><i class="fas fa-hand-point-up"></i> 社團一覽</a>
                         <br>
                         <small>報名時間：({{ $club_semester->start_date2 }} ~ {{ $club_semester->stop_date2 }})</small>                            
