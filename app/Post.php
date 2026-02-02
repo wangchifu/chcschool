a2e8f75fc30f9970511f3850af8193d978dd2a54
wangchifu <wangchifu@hdes.chc.edu.tw>
2026-02-02 11:14:28 +0800

    fix waf

--- app/Post.php
+++ app/Post.php
@@ -30,4 +30,13 @@ class Post extends Model
     {
         return $this->belongsTo(PostType::class);
     }
+
+    public function getContentAttribute($value)
+    {
+        if ($value === null) {
+            return null;
+        }
+        // 移除 https://www.xxx.chc.edu.tw 的 www.
+        return preg_replace('/https?:\/\/www\.([^\/]+\.chc\.edu\.tw)/', 'https://$1', $value);
+    }
 }
