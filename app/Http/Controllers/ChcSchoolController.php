f1a628a0fe7669f9c7d29c7aa9801380410a3e1e
wangchifu <wangchifu@hdes.chc.edu.tw>
2026-07-29 15:57:46 +0800

    fix chcschool

--- app/Http/Controllers/ChcSchoolController.php
+++ app/Http/Controllers/ChcSchoolController.php
@@ -48,6 +48,7 @@ class ChcSchoolController extends Controller
 
         $schools['原斗國中小'] = $schools['原斗國小'];
         $schools['成功高中'] = "50";
+        $school3_1++;
         
         $all_school = [];
         $all_school['彰化市'] = [];
